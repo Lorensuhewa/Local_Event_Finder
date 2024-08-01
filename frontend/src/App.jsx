82bed032b59b91dbc69ac87536019fdb57d9ac9e
Kumesha Rashmi <148706119+KumeshaRashmi@users.noreply.github.com>
2024-08-01 23:32:35 +0530

    create pages and add routes
    home, signup, signin pages created install react-router-dom. change app title index.html page as local event finder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {BrowserRouter,Routes, Route}  from  'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
     

b4c7a49aec46f258b172747ff8fe6ad06a3ccf9d
Kumesha Rashmi <148706119+KumeshaRashmi@users.noreply.github.com>
2024-08-12 00:21:41 +0530

    create signup and login functionality
    created login signup frontend functionality connect database with backend.

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
+
 const app = express();
 
 app.listen(3000, () => {
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,41 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className='flex h-screen'>
       {/* Left side: Logo and Sign-Up Form */}
@@ -12,30 +47,30 @@ export default function SignUp() {
         {/* Sign-Up Form */}
         <div className='max-w-md w-full'>
           <h1 className='text-7xl text-center font-semibold mb-7'>Create an account</h1>
-          <form className='flex flex-col gap-4'>
+          <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
             <input
               type='text'
               placeholder='Username'
               id='username'
               className='bg-slate-100 p-3 rounded-lg'
-              // onChange={handleChange}
+              onChange={handleChange}
             />
             <input
               type='email'
               placeholder='Email'
               id='email'
               className='bg-slate-100 p-3 rounded-lg'
-              // onChange={handleChange}
+              onChange={handleChange}
             />
             <input
               type='password'
               placeholder='Password'
               id='password'
               className='bg-slate-100 p-3 rounded-lg'
-              // onChange={handleChange}
+              onChange={handleChange}
             />
-            <button className='bg-[#f87171] text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-90'>
-              Sign Up
+            <button  disabled={loading} className='bg-[#f87171] text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-90'>
+            {loading ? 'Loading...' : 'Sign Up'}
             </button>
           </form>
           <div className='flex gap-2 mt-5 justify-center'>
@@ -44,6 +79,7 @@ export default function SignUp() {
               <span className='text-blue-500'>Log in</span>
             </Link>
           </div>
+          <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
         </div>
       </div>
 
